3d36bb88a23e86eb982cf823748849a358ef4c00
Grazziano Fagundes <grazzianofagundes@gmail.com>
2023-12-28 12:22:20 -0300

    Event Form - Guests

--- src/app/admin/events/_components/event-form/General.tsx
+++ src/app/admin/events/_components/event-form/General.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Input, Textarea } from '@nextui-org/react';
+import React, { useState } from 'react';
+import { Button, Input, Textarea, Chip } from '@nextui-org/react';
 
 export interface EventFormStepProps {
   event: any;
@@ -8,13 +8,50 @@ export interface EventFormStepProps {
   setActiveStep: React.Dispatch<React.SetStateAction<number>>;
 }
 
-export default function General({ event, setEvent }: EventFormStepProps) {
+export default function General({
+  event,
+  setEvent,
+  activeStep,
+  setActiveStep,
+}: EventFormStepProps) {
+  const [guest, setGuest] = useState<string>('');
+
   const getCommonProps = (name: string) => {
     return {
       labelPlacement: 'outside',
       value: event?.[name],
       onchange: (e: any) => setEvent({ ...event, [name]: e.target.value }),
+      isRequired: true,
     } as any;
+  };
+
+  const onGuestAdd = () => {
+    const newGuests = [];
+    const commaSeparateGuests = guest.split(',');
+
+    // if there are more than one guest in the input, use them
+    if (commaSeparateGuests.length > 1) {
+      newGuests.push(...commaSeparateGuests);
+    } else {
+      // add the single guest
+      newGuests.push(guest);
+    }
+
+    // check if there are already guests in the event
+    if (event?.guests) {
+      newGuests.push(...event.guests);
+    }
+
+    setEvent({ ...event, guests: newGuests });
+    setGuest('');
+  };
+
+  const onGuestRemove = (guestToRemove: number) => {
+    const newGuests = event?.guests?.filter(
+      (_: string, index: number) => index !== guestToRemove
+    );
+
+    setEvent({ ...event, guests: newGuests });
   };
 
   return (
@@ -32,6 +69,36 @@ export default function General({ event, setEvent }: EventFormStepProps) {
       />
 
       <Textarea label="Description" {...getCommonProps('description')} />
+
+      <div className="flex gap-5 items-end">
+        <Input
+          placeholder="Enter your guests"
+          label="Guests"
+          value={guest}
+          onChange={(e) => setGuest(e.target.value)}
+          labelPlacement="outside"
+        />
+        <Button onClick={onGuestAdd}>Add</Button>
+      </div>
+
+      <div className="flex flex-wrap gap-5">
+        {event?.guests?.map((guest: string, index: number) => (
+          <Chip key={index} onClose={() => onGuestRemove(index)}>
+            {guest}
+          </Chip>
+        ))}
+      </div>
+
+      <div className="flex justify-center gap-5">
+        <Button onClick={() => {}}>Cancel</Button>
+        <Button
+          onClick={() => setActiveStep(activeStep + 1)}
+          color="primary"
+          isDisabled={!event?.name || !event?.organizer || !event?.description}
+        >
+          Next
+        </Button>
+      </div>
     </div>
   );
 }
