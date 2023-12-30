65f6f5ee581e27a3c59132474e0a7a1ce708fd9d
Grazziano Fagundes <grazzianofagundes@gmail.com>
2023-12-30 13:22:41 -0300

    Get events

---
+++ src/app/admin/events/_components/events-table/index.tsx
@@ -0,0 +1,51 @@
+'use client';
+import React from 'react';
+import { EventType } from '@/interfaces/events';
+import {
+  Table,
+  TableHeader,
+  TableColumn,
+  TableBody,
+  TableRow,
+  TableCell,
+  Button,
+} from '@nextui-org/react';
+
+export default function EventsTable({ events }: { events: EventType[] }) {
+  return (
+    <div className="mt-5">
+      <Table aria-label="Example static collection table">
+        <TableHeader>
+          {['Name', 'Organizer', 'Date', 'Time', 'Location', 'Actions'].map(
+            (column) => (
+              <TableColumn key={column} className="bg-gray-400 text-white">
+                {column}
+              </TableColumn>
+            )
+          )}
+        </TableHeader>
+        <TableBody>
+          {events.map((event) => (
+            <TableRow key={event._id}>
+              <TableCell>{event.name}</TableCell>
+              <TableCell>{event.organizer}</TableCell>
+              <TableCell>{event.date}</TableCell>
+              <TableCell>{event.time}</TableCell>
+              <TableCell>{event.location}</TableCell>
+              <TableCell>
+                <div className="flex gap-5">
+                  <Button isIconOnly size="sm">
+                    <i className="ri-delete-bin-line"></i>
+                  </Button>
+                  <Button isIconOnly size="sm">
+                    <i className="ri-pencil-line"></i>
+                  </Button>
+                </div>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
+  );
+}
--- src/app/admin/events/page.tsx
+++ src/app/admin/events/page.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import PageTitle from '@/components/PageTitle';
 import { Link } from '@nextui-org/react';
+import EventModel from '@/models/event-model';
+import { EventType } from 'firebase/database';
+import EventsTable from './_components/events-table';
 
-export default function EventsPage() {
+export default async function EventsPage() {
+  const events: EventType[] = (await EventModel.find().sort({
+    createdAt: -1,
+  })) as any;
+
   return (
     <div>
       <div className="flex justify-between">
@@ -15,6 +22,8 @@ export default function EventsPage() {
           Create Event
         </Link>
       </div>
+
+      <EventsTable events={JSON.parse(JSON.stringify(events))} />
     </div>
   );
 }
